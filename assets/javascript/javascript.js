12a3f14965313dd27c5a0822593a63cee29899d4
Tyler G <guthriet1230@gmail.com>
2018-10-09 18:50:56 -0500

    buttons rendered

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,30 +1,51 @@
-////////////////////////////////////////////////////
-// displaying the giphys
-////////////////////////////////////////////////////
+var animals = ["cats", "dogs"]
+
 $("button").on("click", function() {
-  var animal = $(this).attr("data-animal");
-  var queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=" +
-    animal +
-    "&api_key=dc6zaTOxFJmzC&limit=10";
+  var animal = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response);
-    var results = response.data;
-    for (var i = 0; i < results.length; i++) {
+      console.log(response)
+      var results = response.data; 
+
+      for (var i = 0; i < results.length; i++) {
       var animalDiv = $("<div>");
       var p = $("<p>");
       p.text("Rating: " + results[i].rating);
       var animalImage = $("<img>");
       animalImage.attr("src", results[i].images.fixed_height.url);
       animalDiv.append(p);
-
       animalDiv.append(animalImage);
 
       $("#gifs-appear-here").prepend(animalDiv);
-    }
+      }
   });
-});
+})
+
+
+// defining the array, rendering buttons, and adding buttons
+
+function renderButtons() {
+
+  $("#buttons-view").empty();
+  for (var i = 0; i < animals.length; i++) {
+    var a = $("<button>");
+    a.addClass("animals-btn");
+    a.attr("data-name", animals[i]);
+    a.text(animals[i]);
+    $("#buttons-view").append(a);
+  }
+}
+
+$("#add-animal").on("click", function(event) {
+  event.preventDefault();
+  var animall = $("#animal-input").val().trim();
+  animals.push(animall);
+  renderButtons();
+}); 
+
+renderButtons()
